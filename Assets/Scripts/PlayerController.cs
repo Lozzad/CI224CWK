9f4932f19562b9919d814b4cf4bf886674309e36
Lozzad <laurie.dibble1@gmail.com>
2019-05-20 12:48:03 +0100

    the project files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed = 10;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    Rigidbody rb;
+    Camera cam;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //snappier jump
+        if (rb.velocity.y < 0) {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        } else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
+			rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        } 
+        
+        Move();
+
+        if (Input.GetButtonDown("Jump")) {
+            Jump();
+        }
+    }
+
+    void Move() {
+		Vector3 rawMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		Vector3 actualMovement = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0) * rawMovement;
+		transform.position += actualMovement * speed * Time.deltaTime;
+    }
+
+    void Jump() {
+        rb.AddForce(Vector3.up*10, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+    //references to positions needed
+    public Transform lookAt;            //where to look
+    public Transform camTransform;      //the cam transform
+    private Camera cam;                 //the camera
+    private float distance = 10.0f;     //cam distance
+    private float currX = 0.0f;         //current X
+    private float currY = 0.0f;         //current Y
+    private float sensitivityX = 4.0f;  //sensitivity vals
+    private float sensitivityY = 1.0f;  //^^
+
+    private void Start() {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update() {
+        currX += Input.GetAxis("Mouse X")*sensitivityX;
+        currY += Input.GetAxis("Mouse Y")*sensitivityY;
+        currY = Mathf.Clamp(currY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    //use this so the camera reacts to player move
+    private void LateUpdate() {
+        Vector3 dir = new Vector3(0,0,-distance);
+        Quaternion rotation = Quaternion.Euler(currY,currX,0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
